f5f00b407920fad738ff4cf02f58722656b23215
finisterra-io <finisterra-io@users.noreply.github.com>
2024-08-15 00:38:44 +0000

    Automatic scan for iam.aws.429908783399.global

---
+++ finisterra/generated/aws/429908783399/global/iam/import-aws_iam_role-StorageGatewayBucketAccessRole17236718550570_10911149021244237.tf
@@ -0,0 +1,10 @@
+import {
+  id = "StorageGatewayBucketAccessRole17236718550570.10911149021244237"
+  to = module.aws_iam_role-StorageGatewayBucketAccessRole17236718550570_10911149021244237.aws_iam_role.default[0]
+}
+
+import {
+  id = "StorageGatewayBucketAccessRole17236718550570.10911149021244237/arn:aws:iam::429908783399:policy/service-role/AllowStorageGatewayAssumeBucketAccessRole17236718550570.9306023719267222"
+  to = module.aws_iam_role-StorageGatewayBucketAccessRole17236718550570_10911149021244237.aws_iam_role_policy_attachment.managed["StorageGatewayBucketAccessRole17236718550570.10911149021244237_AllowStorageGatewayAssumeBucketAccessRole17236718550570.9306023719267222"]
+}
+
