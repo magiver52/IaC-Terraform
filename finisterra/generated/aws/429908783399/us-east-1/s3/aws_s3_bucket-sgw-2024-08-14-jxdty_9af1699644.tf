3dd996a169b842d120375c24f6c7d5508ad639f6
finisterra-io <finisterra-io@users.noreply.github.com>
2024-08-29 00:40:26 +0000

    Automatic scan for s3.aws.429908783399.us-east-1

--- finisterra/generated/aws/429908783399/us-east-1/s3/aws_s3_bucket-sgw-2024-08-14-jxdty_9af1699644.tf
+++ finisterra/generated/aws/429908783399/us-east-1/s3/aws_s3_bucket-sgw-2024-08-14-jxdty_9af1699644.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "aws_s3_bucket-sgw-2024-08-14-jxdty_9af1699644" {
-  source               = "github.com/finisterra-io/terraform-aws-s3?ref=v1.0.3"
+  source               = "github.com/finisterra-io/terraform-aws-s3?ref=e9178c2b9250d466e55f9b9eceac96238986d7e0"
   attach_public_policy = true
   bucket               = local.bucket_9af1699644
   object_lock_enabled  = false
