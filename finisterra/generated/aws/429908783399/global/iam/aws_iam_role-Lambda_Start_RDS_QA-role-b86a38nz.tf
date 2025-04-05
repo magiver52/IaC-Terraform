88ff1b0cd9f37c891314bce6b386339e784810ca
finisterra-io <finisterra-io@users.noreply.github.com>
2025-04-05 00:46:17 +0000

    Automatic scan for iam.aws.429908783399.global

--- finisterra/generated/aws/429908783399/global/iam/aws_iam_role-Lambda_Start_RDS_QA-role-b86a38nz.tf
+++ finisterra/generated/aws/429908783399/global/iam/aws_iam_role-Lambda_Start_RDS_QA-role-b86a38nz.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "aws_iam_role-Lambda_Start_RDS_QA-role-b86a38nz" {
-  source             = "github.com/finisterra-io/terraform-aws-iam//modules/iam_role?ref=v1.0.2"
+  source             = "github.com/finisterra-io/terraform-aws-iam//modules/iam_role?ref=298005618b90b7e0c6afc7b8f9ffac99c14401df"
   role_name          = local.role_name_cfa2729319
   assume_role_policy = <<EOF
 {
