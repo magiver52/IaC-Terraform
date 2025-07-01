62bec65ef6f1a1c9e1f23476bbdd25272f5154c0
finisterra-io <finisterra-io@users.noreply.github.com>
2025-07-01 01:00:22 +0000

    Automatic scan for logs.aws.429908783399.us-east-1

--- finisterra/generated/aws/429908783399/us-east-1/logs/aws_cloudwatch_log_group-_ecs_Nginx.tf
+++ finisterra/generated/aws/429908783399/us-east-1/logs/aws_cloudwatch_log_group-_ecs_Nginx.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "aws_cloudwatch_log_group-_ecs_Nginx" {
-  source            = "github.com/finisterra-io/terraform-aws-cloudwatch//modules/log-group?ref=v1.0.1"
+  source            = "github.com/finisterra-io/terraform-aws-cloudwatch//modules/log-group?ref=b7a17fd8594b7506cae986aff39b5d16c0123c36"
   name              = local.name_72786b1863
   retention_in_days = 0
 }
