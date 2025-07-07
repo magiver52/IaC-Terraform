90d56b2c7ad28e59312adbd4f309d36fdf83d3ad
finisterra-io <finisterra-io@users.noreply.github.com>
2025-07-07 00:58:08 +0000

    Automatic scan for security_group.aws.429908783399.us-east-1

--- finisterra/generated/aws/429908783399/us-east-1/security_group/aws_security_group-launch-wizard-1_2e44439df2.tf
+++ finisterra/generated/aws/429908783399/us-east-1/security_group/aws_security_group-launch-wizard-1_2e44439df2.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "aws_security_group-launch-wizard-1_2e44439df2" {
-  source                     = "github.com/finisterra-io/terraform-aws-security-group?ref=817fa52ffb233f892f9323fe0b1de6cf22e158cc"
+  source                     = "github.com/finisterra-io/terraform-aws-security-group?ref=v1.0.2"
   security_group_name        = local.security_group_name_2e44439df2
   vpc_id                     = "vpc-0b2ab00e7c6c5cecc"
   security_group_description = "${local.security_group_name_2e44439df2} created 2024-08-14T22:02:47.690Z"
