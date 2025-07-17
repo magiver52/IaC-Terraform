bf20144f0907f0e18063fdca1450db89365563b3
finisterra-io <finisterra-io@users.noreply.github.com>
2025-07-17 00:56:40 +0000

    Automatic scan for logs.aws.429908783399.us-east-1

--- finisterra/generated/aws/429908783399/us-east-1/logs/aws_cloudwatch_log_group-RDSOSMetrics.tf
+++ finisterra/generated/aws/429908783399/us-east-1/logs/aws_cloudwatch_log_group-RDSOSMetrics.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "aws_cloudwatch_log_group-RDSOSMetrics" {
-  source            = "github.com/finisterra-io/terraform-aws-cloudwatch//modules/log-group?ref=v1.0.1"
+  source            = "github.com/finisterra-io/terraform-aws-cloudwatch//modules/log-group?ref=b7a17fd8594b7506cae986aff39b5d16c0123c36"
   name              = local.name_2a0bfffbd4
   retention_in_days = 30
 }
