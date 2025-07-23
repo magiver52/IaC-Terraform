c70089e6bf224c62ead8ed36c1b38e36326cb873
finisterra-io <finisterra-io@users.noreply.github.com>
2025-07-23 00:57:31 +0000

    Automatic scan for iam.aws.429908783399.global

--- finisterra/generated/aws/429908783399/global/iam/aws_iam_role-ManagedInstancesRole.tf
+++ finisterra/generated/aws/429908783399/global/iam/aws_iam_role-ManagedInstancesRole.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "aws_iam_role-ManagedInstancesRole" {
-  source             = "github.com/finisterra-io/terraform-aws-iam//modules/iam_role?ref=298005618b90b7e0c6afc7b8f9ffac99c14401df"
+  source             = "github.com/finisterra-io/terraform-aws-iam//modules/iam_role?ref=v1.0.2"
   role_name          = local.role_name_0479ca1fac
   assume_role_policy = <<EOF
 {
