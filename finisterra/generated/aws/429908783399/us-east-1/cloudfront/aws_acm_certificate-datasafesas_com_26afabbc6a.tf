934159e9389e3dabf48b680057bd74dde39fbbcf
finisterra-io <finisterra-io@users.noreply.github.com>
2025-07-23 00:58:05 +0000

    Automatic scan for cloudfront.aws.429908783399.us-east-1

--- finisterra/generated/aws/429908783399/us-east-1/cloudfront/aws_acm_certificate-datasafesas_com_26afabbc6a.tf
+++ finisterra/generated/aws/429908783399/us-east-1/cloudfront/aws_acm_certificate-datasafesas_com_26afabbc6a.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "aws_acm_certificate-datasafesas_com_26afabbc6a" {
-  source      = "github.com/finisterra-io/terraform-aws-acm?ref=v1.0.1"
+  source      = "github.com/finisterra-io/terraform-aws-acm?ref=d13fadc2a96fbe002aa867d2f0e2ca07433212f3"
   domain_name = local.domain_name_26afabbc6a
   subject_alternative_names = [
     local.domain_name_26afabbc6a,
