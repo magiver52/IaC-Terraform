cfed10936a9e0b0667d87b6affce01eea092ee9d
finisterra-io <finisterra-io@users.noreply.github.com>
2025-07-25 00:56:30 +0000

    Automatic scan for cloudfront.aws.429908783399.us-east-1

--- finisterra/generated/aws/429908783399/us-east-1/cloudfront/aws_cloudfront_distribution-E86TF5AFSS4RU.tf
+++ finisterra/generated/aws/429908783399/us-east-1/cloudfront/aws_cloudfront_distribution-E86TF5AFSS4RU.tf
@@ -1,5 +1,5 @@
 module "aws_cloudfront_distribution-E86TF5AFSS4RU" {
-  source  = "github.com/finisterra-io/terraform-aws-cloudfront?ref=v1.0.2"
+  source  = "github.com/finisterra-io/terraform-aws-cloudfront?ref=6aec91c659dfbc39874375a482093da97b2194d2"
   enabled = true
   aliases = [
     "datasafesas.com",
